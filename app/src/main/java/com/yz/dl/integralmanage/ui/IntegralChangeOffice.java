5a3eeaca320c22ccda38cc870422457e23e59776
I'M CHAMAN <475934874@qq.com>
2017-09-27 17:34:09 +0800

    积分汇总 ui初步

--- app/src/main/java/com/yz/dl/integralmanage/ui/IntegralChangeOffice.java
+++ app/src/main/java/com/yz/dl/integralmanage/ui/IntegralChangeOffice.java
@@ -2,6 +2,10 @@ package com.yz.dl.integralmanage.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.yz.dl.integralmanage.R;
 
 /**
  * Created by I'M CHAMAN on 2017/9/27.
@@ -11,5 +15,7 @@ public class IntegralChangeOffice extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_integralchangeoffice);
     }
 }
