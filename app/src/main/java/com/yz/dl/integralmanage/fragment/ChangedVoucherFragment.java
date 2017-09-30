bb4dce7e8b3e655cd4fba77070d74c8587db7a92
I'M CHAMAN <475934874@qq.com>
2017-09-30 12:53:01 +0800

    代金券兑换  假期兑换

--- app/src/main/java/com/yz/dl/integralmanage/fragment/ChangedVoucherFragment.java
+++ app/src/main/java/com/yz/dl/integralmanage/fragment/ChangedVoucherFragment.java
@@ -7,7 +7,12 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.yz.dl.integralmanage.R;
+import com.yz.dl.integralmanage.adapter.ChangeDetailVoucherAdapter;
 import com.yz.dl.integralmanage.base.BaseFragment;
+import com.yz.dl.integralmanage.bean.AwardListBean;
+import com.yz.dl.integralmanage.comm.Constants;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,10 +25,27 @@ import butterknife.ButterKnife;
 public class ChangedVoucherFragment extends BaseFragment {
     @Bind(R.id.fragment_voucher_listview)
     ListView fragmentVoucherListview;
+    ChangeDetailVoucherAdapter adapter;
+    ArrayList<AwardListBean> arrayList;
 
     @Override
     protected void TODO(View view, Bundle savedInstanceState) {
-
+        arrayList = new ArrayList<>();
+        for (int i = 1; i < 7; i++) {
+            AwardListBean awardListBean = new AwardListBean();
+            if (i % 2 == 0) {
+                awardListBean.setState(Constants.STATE_GIVE);
+            } else {
+                awardListBean.setState(Constants.STATE_CHANGED);
+            }
+            awardListBean.setAward("奖励假期" + i + " 天");
+            awardListBean.setName("章程" + i);
+            awardListBean.setPlace("锦江区 - 城北加油站");
+            awardListBean.setTime("2017-05-09");
+            arrayList.add(awardListBean);
+        }
+        adapter = new ChangeDetailVoucherAdapter(getActivity(),arrayList);
+        fragmentVoucherListview.setAdapter(adapter);
     }
 
     @Override
